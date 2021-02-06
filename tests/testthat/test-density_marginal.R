08e5a1f34ed54f52b73865e0ddeca230cbfcc1e9
Nima Hejazi <nh@nimahejazi.org>
2021-02-05 18:07:58 -0800

    corrections + reducing CV

--- tests/testthat/test-density_marginal.R
+++ tests/testthat/test-density_marginal.R
@@ -7,19 +7,21 @@ a <- rnorm(n_train, 0, 2)
 # learn marginal density of A using HAL regression
 mod_haldensify <- haldensify(
   A = a, W = NULL,
-  n_bins = c(5, 10),
+  n_bins = c(3, 5),
   lambda_seq = exp(seq(-1, -13, length = 200))
 )
 
 # estimate density via Gaussian kernel density
 gauss_dens <- density(a)
-gauss_emprisk <- mean(-log(gauss_dens$y))
 
 # HAL predictions of density using support from the fitted density function
 hal_dens <- predict(mod_haldensify,
   new_A = gauss_dens$x, new_W = rep(0, length(a))
 )
-hal_emprisk <- mean(-log(hal_dens))
+
+# compute empirical risk of each estimator based on -log(P) loss
+hal_emprisk <- mean(-log(hal_dens[hal_dens > 0]))
+gauss_emprisk <- mean(-log(gauss_dens$y)[hal_dens > 0])
 
 # HAL-predicted density matches Gaussian kernel density closely in loss?
 test_that("Empirical risk of HAL density less than that of kernel density", {
